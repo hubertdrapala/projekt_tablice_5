977b764d64139fcadb0e9c703a492af91e9d7a77
hubertdrapala <92692812+hubertdrapala@users.noreply.github.com>
2021-12-07 21:13:26 +0100

    Add files via upload

---
+++ projtek_tablice_5/projtek_tablice_5/projtek_tablice_5.cpp
@@ -0,0 +1,124 @@
+﻿// projekt5.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+	//zad 1
+	//char zdanie[] = { "lot" };// inicjalizacja łancuchów znaków
+
+	//char zdanie[] = { 'l', 'o' 't' }; //inicjalizacja zbiorcza
+
+	//zad 2
+
+	/*int liczby[100];
+	char znaki[100];
+	cout << "podaj liczby: ";
+	cin >> liczby;
+	cin >> znaki;*/
+
+	//zad 3
+
+	/*char tab[] = "wiosna";
+	cout << "rozmiar tablicy: " << size(tab);*/
+
+    //zad4 
+    //inicjalizacja
+    //cout << "inicjalizacja" << endl;
+    //string tekst("Hubert ma kota");
+    //string tekst_drugi(tekst);
+    //cout << tekst << endl << tekst_drugi;
+    //cin.get();
+    //przechowywanie danych
+    //cout << "przechowywanie danych" << endl;
+    //string tekst3 = " tekst";
+    //cout << tekst3 << endl;
+    ////kopiowanie
+    //cout << "kopiowanie" << endl;
+    //string tekst4 ( "Hubert ma kota");
+    //char tab[10];
+
+    //int liczba_znakow = tekst4.copy(tab, 4, 7);
+
+    //tab[liczba_znakow] = 0; 
+    //cout << tab;
+    //cin.get();
+    ////laczenie
+    //cout << "laczenie " << endl;
+    //string tekst5, tekst6, teksty_polaczone;
+    //cout << "Podaj tekst: ";
+    //getline(cin, tekst5);
+    //cout << "Podaj drugi tekst: ";
+    //getline(cin, tekst6);
+    //teksty_polaczone = tekst5 + tekst6;
+    //cout << "Polaczony tekst to: " << teksty_polaczone << endl;
+    ////dlugosc tekstu
+    //cout << "dlugosc tekstu" << endl;
+    //string tekst7("Hubert ma kota");
+    //int liczba_znakow= tekst7.length();
+    //cout << "ilosc znakow " << tekst7.length() << "znakow ";
+
+	
+
+	//zad 5
+	/*int ocena;
+	string nazwa_ulicy, numer_domu, staz;
+	cout << "podaj nazwe ulicy na ktorej mieszakasz: ";
+	getline( cin, nazwa_ulicy);
+	cout << "podaj nr domu: ";
+	cin >> numer_domu;
+	cout << "Na ile oceniasz swoje umiejetnosci programowania w skali [2 - 5]: ";
+	cin >> ocena;
+	ocena = ocena - 1;
+	cout << "twoj staz programistyczny: ";
+	cin >> staz;
+	cout << "Adres: "; cout << nazwa_ulicy; cout << numer_domu << endl;
+	cout << "Ocena: "; cout << ocena << endl;
+	cout << "Staz: "; cout << staz; cout << " dni";*/
+
+
+    //zad 6
+    /*string tekst;
+    cout << "Wprowadz tekst: ";
+    getline(cin, tekst);
+
+    string spolgloski = "bcdfghjklmnpqrstwxzBCDFGHJKLMNPRQSTWXZ";
+    int  n_spolgloski = 0;
+
+    for (int i = 0; i < tekst.length(); i++)
+    {
+
+        for (int j = 0; j < spolgloski.length(); j++)
+        {
+            if (tekst[i] == spolgloski[j])
+                n_spolgloski++;
+        }
+    }
+    string samogloski = "aeiouyAEIOUY";
+    int n_samogloski = 0;
+    for (int i = 0; i < tekst.length(); i++)
+    {
+        for (int j = 0; j < samogloski.length(); j++)
+        {
+            if (tekst[i] == samogloski[j])
+                n_samogloski++;
+        }
+    }
+
+    cout << "Dlugosc calego tekstu to: " << tekst.length() << endl;
+
+    cout << "Spolgloski (suma): " << n_spolgloski << endl;
+    cout << "samogloski (suma): " << n_samogloski << endl;*/
+
+
+
+
+
+
+
+
+
+
+
+}
